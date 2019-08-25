2172a8a56c40f9c99081979c31b31eee60a543a6
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-08-25 15:54:26 -0400

    Update macOS-docker-build-and-deploy.sh

--- macOS-docker-build-and-deploy.sh
+++ macOS-docker-build-and-deploy.sh
@@ -21,5 +21,5 @@ docker run -it -d --network="host" --restart="always" \
 -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 --entrypoint "/sbin/init" \
-c4pt/skytune
+c4pt/skytune-current
 docker exec -it skytunedocker /bin/bash
