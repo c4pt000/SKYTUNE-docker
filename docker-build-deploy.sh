e3842058d0044ff12d5a4e8cbf1cad48e5a0df25
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-08-25 14:31:31 -0400

    Update docker-build-deploy.sh

--- docker-build-deploy.sh
+++ docker-build-deploy.sh
@@ -1,12 +1,30 @@
-#linux dont run as root for pulseaudio, for X11 forwarding via docker check selinux settings or !unsafe lastly setenforce 0 
-docker build -t skytune .
-    
-docker run --network="host" -it --restart="always" \
--e PULSE_SERVER=docker.for.mac.localhost -e DISPLAY=docker.for.mac.host.internal:0 \
+# macos snd 
+# brew install pulseaudio
+# brew services start pulseaudio
+# brew install socat
+# brew cask install xquartz
+# pulseaudio --load=module-native-protocol-tcp --exit-idle-time=-1 --daemon
+
+
+#brew services start pulseaudio
+#pulseaudio --load=module-native-protocol-tcp --exit-idle-time=-1 --daemon
+
+
+#ENV PULSE_SERVER=docker.for.mac.localhost
+
+#socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+
+export XDG_RUNTIME_DIR=/run/user/%I && /usr/lib/systemd/systemd --user
+
+
+docker run -it -d --network="host" --restart="always" --privileged=true \
+--name "skytunedocker" \
+--device /dev/snd \
+-e DISPLAY=$DISPLAY \
 --stop-signal=SIGRTMIN+3 --cap-add=SYS_ADMIN --security-opt=seccomp:unconfined \
 -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
---entrypoint "/bin/bash" \
-c4pt/skytune-current
+--entrypoint "/sbin/init" \
+c4pt/skytune-current 
 
-
+docker exec -it skytunedocker /bin/bash
