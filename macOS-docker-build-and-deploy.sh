81091a430124aba96d23755e19f532e87cfe53e5
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-08-25 12:31:08 -0400

    Update macOS-docker-build-and-deploy.sh

--- macOS-docker-build-and-deploy.sh
+++ macOS-docker-build-and-deploy.sh
@@ -3,6 +3,7 @@ ENV PULSE_SERVER=docker.for.mac.localhost
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 
 docker run -it -d --network="host" --restart="always" \
+--name "skytune-docker" \
 -e PULSE_SERVER=docker.for.mac.localhost -e DISPLAY=docker.for.mac.host.internal:0 \
 --stop-signal=SIGRTMIN+3 --cap-add=SYS_ADMIN --security-opt=seccomp:unconfined \
 -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
@@ -10,5 +11,4 @@ docker run -it -d --network="host" --restart="always" \
 --entrypoint "/sbin/init" \
 c4pt/skytune-current 
 
-
-echo "docker exec -it <image-id> /bin/bash"
+docker exec -it skytune-docker /bin/bash
