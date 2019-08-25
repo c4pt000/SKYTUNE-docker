05ecd064a4fec43696973da1a28267d802c4940a
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-08-25 07:42:32 -0400

    Update macOS-docker-build-and-deploy.sh

--- macOS-docker-build-and-deploy.sh
+++ macOS-docker-build-and-deploy.sh
@@ -2,5 +2,7 @@ ENV PULSE_SERVER=docker.for.mac.localhost
 
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 
-docker run -itd -p 80:80 -p 443:443 -p 8080:8080 --privileged --rm -e PULSE_SERVER=docker.for.mac.localhost -e DISPLAY=docker.for.mac.host.internal:0 \
--v /tmp/.X11-unix:/tmp/.X11-unix c4pt/skytune
+docker run -itd -p 80:80 -p 443:443 -p 8080:8080 --privileged --rm \
+-e PULSE_SERVER=docker.for.mac.localhost -e DISPLAY=docker.for.mac.host.internal:0 \
+-v /tmp/.X11-unix:/tmp/.X11-unix --entrypoint "/opt/SKYTUNE.online/launch-http-NOSSL-firefox-local.sh" \
+c4pt/skytune
