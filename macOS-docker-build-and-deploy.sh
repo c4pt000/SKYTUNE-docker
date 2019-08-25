8b2293290bd86e476efe27260ccf45af0e59b929
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-08-25 07:29:20 -0400

    Update macOS-docker-build-and-deploy.sh

--- macOS-docker-build-and-deploy.sh
+++ macOS-docker-build-and-deploy.sh
@@ -3,4 +3,4 @@ ENV PULSE_SERVER=docker.for.mac.localhost
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 
 docker run -it --rm -e PULSE_SERVER=docker.for.mac.localhost -e DISPLAY=docker.for.mac.host.internal:0 \
--v /tmp/.X11-unix:/tmp/.X11-unix c4pt/skytune-docker
+-v /tmp/.X11-unix:/tmp/.X11-unix c4pt/skytune-docker-current
