ccd38315d4acb22a5cbdf1f10c6c21e6ac4ec44f
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-08-25 06:15:52 -0400

    Update docker-build-deploy.sh

--- docker-build-deploy.sh
+++ docker-build-deploy.sh
@@ -1,5 +1,15 @@
 #linux dont run as root for pulseaudio, for X11 forwarding via docker check selinux settings or !unsafe lastly setenforce 0 
 docker build -t skytune .
-docker run -it --rm --cap-add=SYS_ADMIN -e "container=skytune-docker" DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /sys/fs/cgroup:/sys/fs/cgroup c4pt/skytune-docker
+    
+docker run \
+--rm \ 
+--cap-add=SYS_ADMIN \
+--name skytune \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v /sys/fs/cgroup:/sys/fs/cgroup \
+-it \
+--entrypoint "launch-server-production-NON-ssl" \
+skytune
+
 
 
