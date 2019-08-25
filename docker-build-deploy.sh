ae2b783cee94e9d5e9a6388527f8cb0f52f992a9
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-08-25 11:48:43 -0400

    Update docker-build-deploy.sh

--- docker-build-deploy.sh
+++ docker-build-deploy.sh
@@ -1,15 +1,12 @@
 #linux dont run as root for pulseaudio, for X11 forwarding via docker check selinux settings or !unsafe lastly setenforce 0 
 docker build -t skytune .
     
-docker run \
---rm \ 
---cap-add=SYS_ADMIN \
---name skytune \
+docker run --network="host" -it --restart="always" \
+-e PULSE_SERVER=docker.for.mac.localhost -e DISPLAY=docker.for.mac.host.internal:0 \
+--stop-signal=SIGRTMIN+3 --cap-add=SYS_ADMIN --security-opt=seccomp:unconfined \
+-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
--v /sys/fs/cgroup:/sys/fs/cgroup \
--it \
---entrypoint "/opt/SKYTUNE.online/launch-http-NOSSL-firefox-local.sh" \
-skytune
-
+--entrypoint "/bin/bash" \
+c4pt/skytune-current
 
 
